5ab455ec9f64a0af94856a7e20bc224586592ae5
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-11-21 23:24:40 +0300

    Moving objects

---
+++ 5.9.1/5.9.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+template <typename T>
+void move_vectors(std::vector<T> &a, std::vector<T> &b) {
+    b = std::move(a);
+}
+
+int main()
+{
+    std::vector <std::string> one = { "test_string1", "test_string2" };
+    std::vector <std::string> two;
+    move_vectors(one,two);
+
+    std::cout << two[0]<<'\n'<<two[1];
+    
+    return 0;
+}
